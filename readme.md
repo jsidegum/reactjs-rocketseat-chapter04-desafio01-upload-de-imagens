a373cbfcbde03c98b03a55130e4a4d1eae9749b6
jsidegum <jsidegum@gmail.com>
2022-10-30 17:15:06 -0300

    upfi

---
+++ readme.md
@@ -0,0 +1,33 @@
+## Sobre
+
+Desafio da trilha de ReactJS no Ignite da Rocketseat.
+Upfi é uma plataforma para fazer upload de imagens.
+
+Desafio proposto: https://www.notion.so/Desafio-02-Upload-de-imagens-4cf1c3b1c1ad4a66961b6e48558cc3b8
+Design Figma: https://www.figma.com/file/ZI4KWlYEmudSK5d1cO8Vyf/Desafio-2-M%C3%B3dulo-4-ReactJS-(Copy)?node-id=0%3A1
+
+## Tecnologias utilizadas
+- ReactJS
+- Typescript
+- NextJS
+- FaunaDB
+- imgBB
+- Chakra UI
+- React-Query
+- Axios
+
+## Screenshots:
+<img src="/public/imgs/1.png" style="width:700px;"/>
+<img src="/public/imgs/2.png" style="width:700px;"/>
+<img src="/public/imgs/3.png" style="width:700px;"/>
+
+## Executar o projeto
+- Cadastrar e configurar FaunaDB
+- Cadastrar e configurar ImgBB
+- clonar
+- $ yarn install
+- $ yarn dev
+- acessar localhost:3000
+
+## Executar Teste
+- $ yarn test
---
+++ src/components/Form/FormAddImage.tsx
@@ -0,0 +1,170 @@
+import { Box, Button, Stack, useToast } from '@chakra-ui/react';
+import { useForm } from 'react-hook-form';
+import { useState } from 'react';
+import { useMutation, useQueryClient } from 'react-query';
+
+import { api } from '../../services/api';
+import { FileInput } from '../Input/FileInput';
+import { TextInput } from '../Input/TextInput';
+
+interface FormAddImageProps {
+  closeModal: () => void;
+}
+
+interface NewImageData {
+  url: string
+  title: string
+  description: string
+}
+
+export function FormAddImage({ closeModal }: FormAddImageProps): JSX.Element {
+  const [imageUrl, setImageUrl] = useState('');
+  const [localImageUrl, setLocalImageUrl] = useState('');
+  const toast = useToast();
+
+
+  const acceptedFormatsRegex = /(?:([^:/?#]+):)?(?:([^/?#]*))?([^?#](?:jpeg|gif|png))(?:\?([^#]*))?(?:#(.*))?/g;
+
+  const formValidations = {
+    image: {
+      // TODO REQUIRED, LESS THAN 10 MB AND ACCEPTED FORMATS VALIDATIONS
+      required: 'Arquivo obrigatório',
+      validate: {
+        lessThan10MB: fileList =>
+          fileList[0].size < 10000000 || 'O arquivo deve ser menor que 10MB',
+        acceptedFormats: fileList =>
+          acceptedFormatsRegex.test(fileList[0].type) ||
+          'Somente são aceitos arquivos PNG, JPEG e GIF',
+      },
+    },
+    title: {
+      // TODO REQUIRED, MIN AND MAX LENGTH VALIDATIONS
+      required: 'Título obrigatório',
+      minLength: {
+        value: 2,
+        message: 'Mínimo de 2 caracteres',
+      },
+      maxLength: {
+        value: 20,
+        message: 'Máximo de 20 caracteres',
+      },
+    },
+    description: {
+      // TODO REQUIRED, MAX LENGTH VALIDATIONS
+      required: 'Descrição obrigatória',
+      maxLength: {
+        value: 65,
+        message: 'Máximo de 65 caracteres',
+      },
+    },
+  };
+
+  const queryClient = useQueryClient();
+  const mutation = useMutation(
+    // TODO MUTATION API POST REQUEST,
+    async (image: NewImageData) => {
+      await api.post('/api/images', {
+        ...image,
+        url: imageUrl
+      })
+    },
+    {
+      // TODO ONSUCCESS MUTATION
+      onSuccess: () => {
+        queryClient.invalidateQueries('images')
+      },
+    }
+  );
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState,
+    setError,
+    trigger,
+  } = useForm();
+  const { errors } = formState;
+
+  const onSubmit = async (data: NewImageData): Promise<void> => {
+    try {
+      // TODO SHOW ERROR TOAST IF IMAGE URL DOES NOT EXISTS
+
+      // TODO SHOW SUCCESS TOAST
+      // TODO SHOW ERROR TOAST IF SUBMIT FAILED
+      if (!imageUrl) {
+        toast({
+          status: 'error',
+          title: 'Imagem não adicionada',
+          description:
+            'É preciso adicionar e aguardar o upload de uma imagem antes de realizar o cadastro.',
+        });
+        return;
+      }
+      // TODO EXECUTE ASYNC MUTATION
+      await mutation.mutateAsync(data);
+      toast({
+        status: 'success',
+        title: 'Imagem cadastrada',
+        description: 'Sua imagem foi cadastrada com sucesso.',
+      });
+    } catch {
+      toast({
+        status: 'error',
+        title: 'Falha no cadastro',
+        description: 'Ocorreu um erro ao tentar cadastrar a sua imagem.',
+      });
+    } finally {
+      // TODO CLEAN FORM, STATES AND CLOSE MODAL
+      reset();
+      setImageUrl('');
+      setLocalImageUrl('');
+      closeModal();
+    }
+  }
+
+  return (
+    <Box as="form" width="100%" onSubmit={handleSubmit(onSubmit)}>
+      <Stack spacing={4}>
+        <FileInput
+          setImageUrl={setImageUrl}
+          localImageUrl={localImageUrl}
+          setLocalImageUrl={setLocalImageUrl}
+          setError={setError}
+          trigger={trigger}
+          // TODO SEND IMAGE ERRORS
+          // TODO REGISTER IMAGE INPUT WITH VALIDATIONS
+          error={errors.image}
+          {...register('image', formValidations.image)}
+        />
+
+        <TextInput
+          placeholder="Título da imagem..."
+          // TODO SEND TITLE ERRORS
+          // TODO REGISTER TITLE INPUT WITH VALIDATIONS
+          error={errors.title}
+          {...register('title', formValidations.title)}
+        />
+
+        <TextInput
+          placeholder="Descrição da imagem..."
+          // TODO SEND DESCRIPTION ERRORS
+          // TODO REGISTER DESCRIPTION INPUT WITH VALIDATIONS
+          error={errors.description}
+          {...register('description', formValidations.description)}
+        />
+      </Stack>
+
+      <Button
+        my={6}
+        isLoading={formState.isSubmitting}
+        isDisabled={formState.isSubmitting}
+        type="submit"
+        w="100%"
+        py={6}
+      >
+        Enviar
+      </Button>
+    </Box>
+  );
+}
